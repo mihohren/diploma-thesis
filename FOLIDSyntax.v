f7413980d8f8b7f9000b9e162a35cf1f31ce5250
Miho Hren <miho@fer.edu>
2024-03-21 10:11:55 +0100

    Syntax: define signature, term and FOL-formula.
    Added definition of a first-order signature and a term. These
    definitions hold in FOL and FOL_ID. The defined formula type is a
    FOL-formula. To make it into a FOL_ID-formula, we need regular
    relational symbols and inductively defined relational symbols.
    
    Literature:
    * M. Vuković - Matematička logika
    * J. Brotherston - Cyclic proofs for first order logic with inductive
    definitions

---
+++ FOLIDSyntax.v
@@ -0,0 +1,84 @@
+Require Import Coq.Unicode.Utf8.
+Require Coq.Vectors.Vector.
+
+Record Signature := mkSignature {
+                        FuncS : Type;
+                        FuncArr : FuncS -> nat;
+                        RelS : Type;
+                        RelArr : RelS -> nat
+                      }.
+
+Arguments FuncArr [_].
+Arguments RelArr [_].
+
+Section Peano.
+  Inductive PeanoFuncT := o | s | plus | mult.
+  Definition PeanoFuncArr (f : PeanoFuncT) : nat :=
+    match f with
+    | o => 0
+    | s => 1
+    | plus => 2
+    | mult => 2
+    end.
+
+  Inductive PeanoRelT := eq.
+  Definition PeanoRelArr (f : PeanoRelT) : nat :=
+    match f with
+    | eq => 2
+    end.
+
+  Definition PeanoSig : Signature :=
+    {|
+      FuncS := PeanoFuncT;
+      FuncArr := PeanoFuncArr;
+      RelS := PeanoRelT;
+      RelArr := PeanoRelArr
+    |}.
+End Peano.
+
+Module V := Vector.
+Definition Vec := V.t.
+Print V.t.
+
+Section term.
+  Variable σ : Signature.
+
+  Inductive term : Type :=
+  | var : nat -> term
+  | func : forall f : FuncS σ, Vec term (FuncArr f) -> term.
+End term.
+
+Arguments V.nil {A}.
+Arguments V.cons {A} h {n}.
+
+Section term_examples.
+  Example peano_zero :=
+    func PeanoSig o V.nil.
+
+  Example peano_one :=
+    func PeanoSig s (V.cons peano_zero V.nil).
+
+  Example peano_plus :=
+    func PeanoSig plus (V.cons peano_zero (V.cons peano_one V.nil)).
+End term_examples.
+(* As we can see, manually defining terms is a bit clunky.
+   The problem is, if we define a signature to be implicit for
+   [var] and [func], Coq can not deduce that [o] is of type
+   [FuncS PeanoSig] - it thinks that [o] is of type [PeanoFuncT]
+   (and indeed it is). Perhaps a [Coercion] would help us here.
+ *)
+
+Section formula.
+  Variable σ : Signature.
+
+  (* A FOL formula.  To make this a FOL_ID formula,
+     we need to differentiate between normal relational symbols
+     and inductive relational symbols. *)
+  Inductive formula : Type :=
+  | formRel : forall R : RelS σ, Vec (term σ) (RelArr R) -> formula
+  | formAnd : formula -> formula -> formula
+  | formOr  : formula -> formula -> formula
+  | formImp : formula -> formula -> formula
+  | formNeg : formula -> formula
+  | formAll : (nat -> formula) -> formula.
+End formula.
