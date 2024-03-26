17da4ec62b947a6007df52ce140802db6f7243bc
Miho Hren <miho@fer.edu>
2024-03-26 14:45:09 +0100

    Syntax: Term: proved some lemmas, admitted some other lemmas.
    The problem with the admitted lemmas is they require induction over a
    dependent [args]. When I try to use the [induction] tactic, I get some
    crazy error which I do not fully understand.

--- Term.v
+++ Term.v
@@ -40,9 +40,6 @@ Section term.
     | TFunc f args => TFunc f (map (fun st => term_var_subst st u x) args)
     end.
 
-  Notation "t [ u / x ]" := (term_var_subst t u x)
-                              (no associativity, at level 150).
-
   (* The set of all variables of a given term. *)
   Inductive Var : term -> Ensemble var :=
   | VarVar : forall (v : var), Var (TVar v) v
@@ -55,3 +52,49 @@ Arguments TFunc {Σ} _.
 Arguments term_better_ind {Σ} _ _ _ _.
 Arguments term_var_subst {Σ} _ _ _.
 Arguments Var {Σ} _.
+
+Notation " t '[' x '=>' u ']' " := (term_var_subst t u x) (at level 100).
+
+Section term_facts.
+  Variable Σ : signature.
+
+  Lemma var_not_in_Var_not_eq : forall v x, ~ In _ (@Var Σ( TVar v)) x -> v <> x.
+  Proof.
+    intros v x H. intros eq. apply H. subst; constructor.
+  Qed.
+
+  Lemma var_not_in_Func_not_in_sub :
+    forall (f : FuncS Σ) (args : vec (term Σ) (FuncArr f)) (x : var),
+      ~ In _ (Var (TFunc f args)) x -> Vector.Forall (fun st => ~ In _ (Var st) x) args.
+  Proof.
+    intros f args x H.
+    unfold In in *.
+    generalize dependent args.
+    Fail induction args.
+  Admitted.
+    
+  Lemma term_var_subst_id : forall (t : term Σ) (x : var),
+      t [x => TVar x] = t.
+  Proof.
+    induction t as [v | f args IH] using term_better_ind; intros x.
+    - simpl. destruct (v =? x) eqn:E.
+      + f_equal. symmetry. apply Nat.eqb_eq. apply E.
+      + reflexivity.
+    - cbn. f_equal. induction IH.
+      + reflexivity.
+      + simpl; f_equal.
+        * apply H.
+        * apply IHIH.
+  Qed.
+
+  Lemma term_var_subst_no_occ : forall (t u : term Σ) (x : var),
+      ~ In _ (Var t) x -> t [x => u] = t.
+  Proof.
+    intros t; induction t as [v | f args IH] using term_better_ind;
+      intros u x x_not_in_t.
+    - simpl; destruct (v =? x) eqn:Eqvx.
+      + exfalso; apply x_not_in_t.
+        rewrite Nat.eqb_eq in Eqvx. subst; constructor.
+      + reflexivity.
+  Admitted.
+End term_facts.
