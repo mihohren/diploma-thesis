8ff31d530ec7433e68e404e974d06279f95ccbc8
Miho Hren <miho.hren@gmail.com>
2024-04-16 18:55:59 +0200

    Ind: proved some propositions and lemmas.
    TODO: prove the "prefixed point" part of Lemma 2.2.11

--- theories/InductiveDefinitions.v
+++ theories/InductiveDefinitions.v
@@ -1,4 +1,5 @@
 Require Import Base Syntax Semantics.
+Require Import Arith.
 
 Section inductive_definition_set.
   Context {Σ : signature}.
@@ -34,12 +35,12 @@ Section definition_set_operator.
     : Prop :=
         exists (ρ : env M),
         (forall Q us, List.In (existT _ Q us) (preds pr) ->
-                 interpP Q (V.map (fun u => eval ρ u) us))
+                 interpP Q (V.map (eval ρ) us))
         /\
           ( forall P ts, List.In (existT _ P ts) (indpreds pr) ->
                     args P (V.map (eval ρ) ts))
         /\
-          ds = V.map (fun v => eval ρ v) (indargs pr).
+          ds = V.map (eval ρ) (indargs pr).
 
   Definition φ_P (* φ_i *)
     (P : IndPredS Σ)
@@ -52,8 +53,6 @@ Section definition_set_operator.
     rewrite Heq in ds.
     exact (φ_pr pr args ds).
   Defined.
-  
-  Print φ_P.
   
   Definition φ_Φ (args : forall P : IndPredS Σ, vec D (indpred_ar P) -> Prop)
     : forall P : IndPredS Σ, vec D (indpred_ar P) -> Prop :=
@@ -74,8 +73,6 @@ Section definition_set_operator.
     - intros PP ts HIn. apply Hle. apply Hindpreds; auto.
     - apply Hcons.
   Qed.
-  (* OK, dokaz prolazi, ali ovo je unreadable... *)
-  
 End definition_set_operator.
 
 Section approximants.
@@ -83,19 +80,104 @@ Section approximants.
   Context {Φ : @IndDefSet Σ}.
   Let D := domain M.
 
-  Fixpoint φ_Φ_n (α : nat) P (v : vec D (indpred_ar P)) : Prop :=
+  Fixpoint φ_Φ_n P (α : nat) (v : vec D (indpred_ar P)) : Prop :=
     match α with
     | 0 => False
-    | S α => @φ_Φ Σ M Φ (φ_Φ_n α) P v
+    | S α => @φ_Φ Σ M Φ (fun P => φ_Φ_n P α) P v
     end.
 
-  Definition approximant_of (P : IndPredS Σ) (α : nat)
-    : vec D (indpred_ar P) -> Prop :=
-    φ_Φ_n α P.
+  Corollary (* 2.2.10 *) cor : forall α P v,
+      φ_Φ_n P (S α) v <-> @φ_Φ Σ M Φ (fun P => φ_Φ_n P α) P v.
+  Proof.
+    intros α P v. cbn; split; auto.
+  Qed.
+    
+  Definition approximant_of (P : IndPredS Σ)
+    : nat -> vec D (indpred_ar P) -> Prop :=
+    φ_Φ_n P.
+
+  Lemma zeroth_approximant_empty :
+    forall P v, ~approximant_of P 0 v.
+  Proof.
+    intros P v Happrox; inversion Happrox.
+  Qed.
+
+  Proposition (* 2.2.9 *) approximant_succ : forall α P v, approximant_of P α v -> approximant_of P (S α) v.
+  Proof.
+    unfold approximant_of; induction α; intros P v H.
+    - inversion H.
+    - apply φ_Φ_monotone with (fun P => φ_Φ_n P α).
+      + intros Q u H'. cbn. apply IHα. apply H'.
+      + apply H.
+  Qed.
+  
+  Lemma approximant_monotone : forall α β, α < β -> forall P v, approximant_of P α v -> approximant_of P β v.
+  Proof.
+    unfold approximant_of. intros α β Hle P v Hφ.
+    induction Hle.
+    - now apply approximant_succ.
+    - now apply approximant_succ.
+  Qed.  
+      
+  Lemma approximant_characterization : forall α P v,
+      φ_Φ_n P α v <-> exists β, β < α /\ @φ_Φ Σ M Φ (fun P => φ_Φ_n P β) P v.
+  Proof.
+    intros α P v; split; intros H.
+    - induction α.
+      + inversion H.
+      + exists α; split; [apply le_n | apply H].
+    - destruct H as [β [Hle Hφ]]; induction Hle.
+      + apply Hφ.
+      + now apply approximant_succ.
+  Qed.
+
+  Lemma lema_iza_cor_2_2_10 :
+    forall α, (forall P v, φ_Φ_n P (S α) v <-> φ_Φ_n P α v) <->
+           (forall P v, @φ_Φ Σ M Φ (fun P => φ_Φ_n P α) P v -> φ_Φ_n P α v).
+  Proof.
+    intros α; split; intros H.
+    - intros P v H1. apply H. apply H1.
+    - intros P v; split.
+      + intros H1. apply H. apply H1.
+      + intros H1. cbn. now apply approximant_succ.
+  Qed.
+  
+  Definition φ_Φ_ω P v := exists α, φ_Φ_n P α v.
+
+  Section lemma_2_2_11.
+    Lemma ω_prefixed : forall P v, @φ_Φ Σ M Φ φ_Φ_ω P v -> φ_Φ_ω P v.
+    Proof.
+      intros P v H. unfold φ_Φ_ω in *.
+      unfold φ_Φ, φ_P in H.
+      destruct H as (pr & [Heq Hpr] & (ρ & H1 & H2 & H3)).
+      subst P; unfold eq_rect in H3; subst v.
+      (* k := max od svih razina koje postoje zbog H2 *)
+      (*      a jer je lista indpreds pr konacna, takav broj postoji *)
+      (* onda postoji k + 2 *)
+      set (k := 1).
+    Admitted.
+
+    Lemma ω_least : forall args, (forall P v, @φ_Φ Σ M Φ args P v -> args P v) ->
+                            forall P v, φ_Φ_ω P v -> args P v.
+    Proof.
+      intros args (* prefixed point *) Hprefixed P v Hω.
+      destruct Hω as [α Hφ].
+      enough (H : forall m, @φ_Φ Σ M Φ (fun P => φ_Φ_n P m) P v ->
+                       @φ_Φ Σ M Φ args P v).
+      - apply Hprefixed. apply H with α.
+        apply approximant_succ. apply Hφ.
+      - intros m. apply φ_Φ_monotone. induction m; intros Q u HQ.
+        + inversion HQ.
+        + apply Hprefixed. eapply φ_Φ_monotone.
+          2:{ apply HQ. }
+          intros. cbn in *. fold φ_Φ_n in H. apply IHm. apply H.
+    Qed.    
+  End lemma_2_2_11.
 End approximants.
 
 Definition standard_model
   (Σ : signature) (Φ: @IndDefSet Σ)
   : structure Σ -> Prop :=
   fun M =>
-    forall (P : IndPredS Σ) ts, interpIP P ts <-> exists α, @φ_Φ_n Σ M Φ α P ts.
+    forall (P : IndPredS Σ) ts, interpIP P ts <-> exists α, @φ_Φ_n Σ M Φ P α ts.
+
